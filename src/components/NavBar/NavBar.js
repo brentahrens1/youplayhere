5be9026e978b9f413b8b298c501e4f2d25d4367b
Brent Ahrens <cidida@gmail.com>
2020-08-24 12:45:07 -0700

    adds polished desktop nav

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -11,13 +11,18 @@ const NavBar = () => {
                 <li className="nav__list-item">Home</li>
                 <li className="nav__list-item dropdown-trigger">About
                     <ul className="nav__dropdown">
-                        <li>What's You Play Here?</li>
-                        <li>Protagonist</li>
-                        <li>More about Ines / Contact</li>
+                        <li className="nav__dropdown-item">What's You Play Here?</li>
+                        <li className="nav__dropdown-item">Protagonist</li>
+                        <li className="nav__dropdown-item">More about Ines / Contact</li>
                     </ul>
                 </li>
                 <li className="nav__list-item">News</li>
             </ul>
+            <div className="hamburger">
+                <div className="hamburger__bar" />
+                <div className="hamburger__bar" />
+                <div className="hamburger__bar" />
+            </div>
         </div>
     )
 }
