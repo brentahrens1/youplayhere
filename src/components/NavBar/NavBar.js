93159742c91caa6c39150058b1825567684c3e03
Brent Ahrens <cidida@gmail.com>
2020-08-24 14:06:43 -0700

    adds overlay functionality

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import '../../sass/_navbar.scss'
 
 const NavBar = () => {
+    const [ isOpen, setIsOpen ] = useState(false)
     return (
         <div className="nav">
             <div className="nav__logo">
@@ -18,10 +19,23 @@ const NavBar = () => {
                 </li>
                 <li className="nav__list-item">News</li>
             </ul>
-            <div className="hamburger">
+            <div className={`hamburger ${isOpen ? 'open' : ''}`} onClick={() => setIsOpen(!isOpen)}>
                 <div className="hamburger__bar" />
                 <div className="hamburger__bar" />
                 <div className="hamburger__bar" />
+            </div>
+            <div className={`overlay ${isOpen ? 'show' : ''}`}>
+                <ul className="overlay__list">
+                    <li className="overlay__item">Home</li>
+                    <li className="overlay__item overlay__trigger">About
+                        <ul className="overlay__dropdown">
+                            <li className="overlay__dropdown-item">What's You Play Here?</li>
+                            <li className="overlay__dropdown-item">Protagonist</li>
+                            <li className="overlay__dropdown-item">More about Ines / Contact</li>
+                        </ul>
+                    </li>
+                    <li className="overlay-item">News</li>
+                </ul>
             </div>
         </div>
     )
