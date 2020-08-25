3c3a41d5652ad21af0e8fe69e49d256256ef2586
Brent Ahrens <cidida@gmail.com>
2020-08-24 17:37:25 -0700

    adds overlay functionality

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,6 +3,10 @@ import '../../sass/_navbar.scss'
 
 const NavBar = () => {
     const [ isOpen, setIsOpen ] = useState(false)
+    const [ isMobileDropdown, setMobileDropdown ] = useState(false)
+
+    window.onresize = () => (window.innerWidth > 650 && isOpen) && setIsOpen(false)
+
     return (
         <div className="nav">
             <div className="nav__logo">
@@ -27,14 +31,14 @@ const NavBar = () => {
             <div className={`overlay ${isOpen ? 'show' : ''}`}>
                 <ul className="overlay__list">
                     <li className="overlay__item">Home</li>
-                    <li className="overlay__item overlay__trigger">About
-                        <ul className="overlay__dropdown">
+                    <li className={`overlay__item ${isMobileDropdown ? 'overlay__trigger-active' : 'overlay__trigger' }`} onClick={() => setMobileDropdown(!isMobileDropdown)}>About
+                        <ul className={`overlay__dropdown ${isMobileDropdown ? 'overlay__dropdown-show' : ''}`}>
                             <li className="overlay__dropdown-item">What's You Play Here?</li>
                             <li className="overlay__dropdown-item">Protagonist</li>
                             <li className="overlay__dropdown-item">More about Ines / Contact</li>
                         </ul>
                     </li>
-                    <li className="overlay-item">News</li>
+                    <li className="overlay__item">News</li>
                 </ul>
             </div>
         </div>
