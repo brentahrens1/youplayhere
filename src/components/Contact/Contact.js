124f2c5ce3b4ad373447feb0e45123658305b14e
Brent Ahrens <cidida@gmail.com>
2020-09-07 08:05:13 -0700

    adds veriables and mixins to condense scss

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,7 +4,14 @@ import '../../sass/_contact.scss'
 const Contact = () => {
     return (
         <div className="contact">
-
+            <div className="page-nav-background" />
+            <div className="contact__content">
+                <h2>Contact</h2>
+                <p>INÉS VOGELFANG is a film and TV editor from Buenos Aires, Argentina currently living and working in NYC. She received her undergraduate degree in Sound and Image Design at the University of Buenos Aires, and in 2017, received her Masters in Media Studies and Documentary Film at The New School. In 2018 she completed the Art Of Editing Six Week Class at The Edit Center. The most recent short docs she directed and edited "True Fútbol" and "A Selfie With Lady Liberty" were screened at International Film Festivals such as DOCNYC, Corto Circuito in NYC, The Americas Film Festival, and Offside Film Fest. A fiction short she edited "Leo's Shoulder" by Carlos Ledesma, was screened at BAFICI, amongst other prestigious International Film Festivals. She is an alumna of the Imagine Science Film Festival's Symbiosis Competition where she created her film "Duality", and an alumna of the Documentary WIP Lab at DCTV with moderator Yance Ford. She is currently editing a feature documentary ("Untitled Barry Gibbs Documentary” https://www.barrygibbsfilm.com), Executive Produced by Katie Couric, Tony Goldwyn, and Sanaa Hamri, Produced by Park Pictures, and Directed by Zoe Potkin. She speaks Spanish, French, Portuguese and English, which enables her to edit projects all over the world.</p> 
+                <p>You can see more work by Inés by visiting her IMDB or Behance Page</p>
+                <h3>CONTACT ME AT</h3>
+                <p><a href="mailto:ivogelfang@gmail.com" target="_blank">ivogelfang@gmail.com</a></p>
+            </div>
         </div>
     )
 }
