dbf3a7e1e6960d74a7e0c81b47be35d3c00db6f0
Brent Ahrens <cidida@gmail.com>
2020-09-27 20:50:05 -0700

    adds symbols to the map

--- src/components/PlayHere/PlayHere.js
+++ src/components/PlayHere/PlayHere.js
@@ -3,14 +3,20 @@ import '../../sass/_play-here.scss'
 
 // symbols
 
-import chinatownLong from '../../assets/images/symbols/Chinatownlong.png'
-import chinatownLong2 from '../../assets/images/symbols/Chinatownlong2.png'
-import cricket1 from '../../assets/images/symbols/Cricket1.png'
-import sunset from '../../assets/images/symbols/sunset.png'
-
 import cricketBug from '../../assets/images/symbols/Cricketbugwhite.png'
 import cricketWhite from '../../assets/images/symbols/Cricketwhite.png'
 import squirrel from '../../assets/images/symbols/Squirrel.png'
+import apple from '../../assets/images/symbols/apple.png'
+import bench from '../../assets/images/symbols/bench.png'
+import bridge from '../../assets/images/symbols/bridge.png'
+import cup from '../../assets/images/symbols/cup.png'
+import globe from '../../assets/images/symbols/globe.png'
+import location from '../../assets/images/symbols/Location.png'
+import music from '../../assets/images/symbols/music (2).png'
+import pigeon from '../../assets/images/symbols/Pigeon.png'
+import pizza from '../../assets/images/symbols/pizza.png'
+import rat from '../../assets/images/symbols/rat.png'
+import sunset from '../../assets/images/symbols/sunset-park-bid_0.jpg'
 
 // maps
 
@@ -48,6 +54,36 @@ const PlayHere = () => {
                 </div>
                 <div className="play__grid-piece symbol symbol-three">
                     <img src={squirrel} alt="Uptown" />
+                </div>
+                <div className="play__grid-piece symbol symbol-four">
+                    <img src={apple} alt="Uptown" />
+                </div>
+                <div className="play__grid-piece symbol symbol-five">
+                    <img src={rat} alt="Uptown" />
+                </div>
+                <div className="play__grid-piece symbol symbol-six">
+                    <img src={music} alt="Uptown" />
+                </div>
+                <div className="play__grid-piece symbol symbol-seven">
+                    <img src={pigeon} alt="Uptown" />
+                </div>
+                <div className="play__grid-piece symbol symbol-eight">
+                    <img src={bridge} alt="Uptown" />
+                </div>
+                <div className="play__grid-piece symbol symbol-nine">
+                    <img src={location} alt="Uptown" />
+                </div>
+                <div className="play__grid-piece symbol symbol-ten">
+                    <img src={cup} alt="Uptown" />
+                </div>
+                <div className="play__grid-piece symbol symbol-eleven">
+                    <img src={globe} alt="Uptown" />
+                </div>
+                <div className="play__grid-piece symbol symbol-twelve">
+                    <img src={bench} alt="Uptown" />
+                </div>
+                <div className="play__grid-piece symbol symbol-thirteen">
+                    <img src={location} alt="Uptown" />
                 </div>
             </div>
         </div>
