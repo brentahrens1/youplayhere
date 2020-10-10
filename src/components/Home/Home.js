fd7a7e7c3e74c97637bc2436a6ff8e3c5f2a5295
Brent Ahrens <brentahrens@Brents-MacBook-Pro.local>
2020-10-10 14:56:29 -0700

    adds video landing page and animations on content

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,17 +3,28 @@ import '../../sass/_home.scss'
 
 import { Link } from 'react-router-dom'
 
+import landingVideo from '../../assets/LandingVideo/landing.mp4'
+
 const Home = () => {
     return (
         <div className="home">
             <div className="home__overlay">
                 <div className="home__content">
                     <h1>You Play Here</h1>
-                    <p>Interactive map and mini documentaries by Ines Vogelfang</p>
-                    <button><Link to="/play">Play</Link></button>
+                    <h2>Ines Vogelfang | Interactive Map</h2>
+                    <p>
+                       As Immigrants, how do we find our communities? 
+                       Can it be through a recreational activity in a public space? / In this
+                       project you'll hear me trying to find the answer while you navigate 
+                       through a made up map of New York City's public spaces
+                    </p>
+                    <button><h2><Link to="/play">Play Here &#62;&#62;</Link></h2></button>
                 </div>
             </div>
             <div className="home__inner">
+                <video autoPlay muted loop className="landing-video">
+                    <source src={landingVideo} type="video/mp4" />
+                </video>
             </div>
         </div>
     )
