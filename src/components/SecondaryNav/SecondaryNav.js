c7c67ed6dfcdfae292a3c7b06879872d018d86ca
Brent Ahrens <cidida@gmail.com>
2020-10-23 15:01:59 -0700

    full hamburger transition on click

--- src/components/SecondaryNav/SecondaryNav.js
+++ src/components/SecondaryNav/SecondaryNav.js
@@ -5,25 +5,26 @@ import { Link } from 'react-router-dom'
 
 const SecondaryNav = () => {
     const [ isOpen, setIsOpen ] = useState(false)
+    
     return (
         <div className="secondary-nav">
             <div className="nav__logo">
                 <h2><Link to="/play">You Play Here</Link></h2>
             </div>
-            <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
-                <div className="hamburger__bar" />
-                <div className="hamburger__bar" />
-                <div className="hamburger__bar" />
+            <div className={`hamburger ${isOpen ? "open" : ""}`} onClick={() => setIsOpen(!isOpen)}>
+                <div className={`hamburger__bar ${isOpen ? "active" : "closed"}`} />
+                <div className={`hamburger__bar ${isOpen ? "active" : "closed"}`} />
+                <div className={`hamburger__bar ${isOpen ? "active" : "closed"}`} />
             </div>
             <div className={`overlay ${isOpen ? "show" : ""}`}>
                 <ul className="overlay__list">
                     <li className="nav__list-item"><Link to="/about">About</Link></li>
                     <li className="nav__list-item"><Link to="/contact">Contact</Link></li>
                 </ul>
-                <div className="close" onClick={() => setIsOpen(!isOpen)}>
+                {/* <div className="close" onClick={() => setIsOpen(!isOpen)}>
                     <div className="close__bar" />
                     <div className="close__bar" />
-                </div>
+                </div> */}
             </div>
         </div>
     )
